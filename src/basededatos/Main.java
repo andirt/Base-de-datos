a81c657db31cbca0bcbe13aee088b57ad5b1a2ae
Vespertino <Vespertino@C08PC25.informatica.comercio>
2023-05-10 19:16:35 +0200

    Commit final base de datos

--- src/basededatos/Main.java
+++ src/basededatos/Main.java
@@ -16,11 +16,17 @@ public class Main {
         RegistroUsuario user4 = new RegistroUsuario(4, "Aitor", "Menta", "665452Z", "aitormentafuerte@gmail.com", 27);
         RegistroUsuario user5 = new RegistroUsuario(5, "Oscar", "Nivoro", "112542Z", "oscarnivoro@gmail.com", 54);
 
-        /*TablaUsuario.insertarUsuario(user1, "users.dat");
-            RegistroUsuario[] aRegistroUsuario = {user2, user3, user4, user5};
-            TablaUsuario.insertarUsuario(aRegistroUsuario, "tablausuarios.dat");*/
+        //Inserta un solo usuario
+        //TablaUsuario.insertarUsuario(user1, "tablausuarios.dat");
+        
+        //Inserta un array de usuarios
+        //RegistroUsuario[] aRegistroUsuario = {user2, user3, user4, user5};
+        //TablaUsuario.insertarUsuario(aRegistroUsuario, "tablausuarios.dat");
+        
+        //Lee los usuarios del archivo que contiene el array
         RegistroUsuario[] aUsuarios = TablaUsuario.leerRegistroUsuario("tablausuarios.dat");
         System.out.println(Arrays.toString(aUsuarios));
+        
         //RegistroUsuario[] arrayEncontrados = TablaUsuario.busquedaUsuario("tablausuarios.dat", "Aitor");
         //System.out.println(Arrays.toString(arrayEncontrados));
     }
